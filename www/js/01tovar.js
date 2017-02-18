110b358f59cc5a85e3cafccb91858e122438e446
chernykalexander <boxtab@bk.ru>
2017-02-18 18:13:07 +0300

    Clear commit

--- www/js/01tovar.js
+++ www/js/01tovar.js
@@ -1,12 +1,5 @@
 $( document ).ready( function() {
 
-    // console.log( 'Hi hi hi' );
-    
-    // var myvar = null;
-    // var TovarID = null;
-    // var TovarDescr = null;
-    // var TovarPrice = null;
-
     var tovar = {
         
         // Поля таблицы БД
@@ -101,45 +94,17 @@ $( document ).ready( function() {
         $( '#button_update' ).removeAttr( 'disabled' );
         $( '#button_delete' ).removeAttr( 'disabled' );
 
-        // Считываем значения из html таблицы
-        // tovar.id = $( this ).find( 'td:eq(0)' ).html();
-        // tovar.descr = $( this ).find( 'td:eq(1)' ).html();
-        // tovar.price = $( this ).find( 'td:eq(2)' ).html();
-
         // Получить данные из текущей строки таблицы и записать их в объект товар
         tovar.getTable( this );
         // Из объекта товар записать в форму
         tovar.pushForma();
 
-        // myvar = String( TovarID ) + " - " + String( TovarDescr ) + " - " + String( TovarPrice );
-
         // Вывести в консольсодержимое объекта товар
         tovar.writeConsole();
-
-        // Записываем эти значения в поля формы
-        // $( "input[name='id_input']" ).val( tovar.id );
-        // $( "input[name='descr_input']" ).val( tovar.descr );
-        // $( "input[name='price_input']" ).val( tovar.price );
-        
+     
         // console.log( myvar );
     } );
     
-
-    // Кнопки updata и delete активны только при выделенной записи
-    // if ( $( 'table#dbtable' ).hasClass( 'marked' ) ) {
-    //     console.log( 'Marked - найден' );
-    //     // $( '#button_insert' ).removeAttr( 'disabled' );
-    //     // $( '#button_update' ).removeAttr( 'disabled' );
-    //     // $( '#button_delete' ).removeAttr( 'disabled' );
-    //     $( '#button_insert' ).attr( 'disabled', false );
-    //     $( '#button_update' ).attr( 'disabled', false );
-    //     $( '#button_delete' ).attr( 'disabled', false );
-    // } else {
-    //     console.log( 'Нету маркед' );
-    //     $( '#button_insert' ).attr( 'disabled', false );
-    //     $( '#button_update' ).attr( 'disabled', true );
-    //     $( '#button_delete' ).attr( 'disabled', true );
-    // };
 
 
     // ************************************************************************************
@@ -205,21 +170,6 @@ $( document ).ready( function() {
         // Добавление строки в конец таблицы
         
         $( '#dbtable' ).append( '<tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>' );
-
-        // $( '#dbtable tr' ).innerHTML( 'TETSTSTS' );
-
-        // var newLi = document.createElement( 'tr' );
-        // newLi.innerHTML = '<tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>';
-        // dbtable.appendChild(newLi);
-
-        // $( '#dbtable' )[ 0 ].insertAdjacentHTML( 'beforeend', '<tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>' );
-        // $( '#dbtable' ).append( '<tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>' );
-        // $( '#dbtable' ).append( '<tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>' );
-        // $( '#dbtable tr:last' ).after( '<tr></tr>' );
-        // $( '#dbtable tr:last' ).innerHTML = '<tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>';
-        // $( '#dbtable tr:last' ).innerHTML += '<tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>';
-        // $( '#dbtable tr:last' ).after( '<tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>' );
-        // $( '#dbtable tr:last' ).append( '<tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>' ).show();
         
         // Красим строку
         $( '#dbtable tr' ).removeClass( 'marked' );
@@ -290,13 +240,7 @@ $( document ).ready( function() {
         $( '#button_update' ).attr( 'disabled', true );
         $( '#button_delete' ).attr( 'disabled', true );
 
-        // Заполняем поля формы
-        // $( '#id_input' ).val( tovar.id );
-        // $( '#descr_input' ).val( tovar.descr );
-        // $( '#price_input' ).val( tovar.price );
-
         tovar.pushForma();
-
 
         $( '#form_insert_update' ).show();
 
@@ -395,7 +339,6 @@ $( document ).ready( function() {
             $( '#button_delete' ).attr( 'disabled', true );
 
             $( '.marked' ).remove();
-            // $( '#dbtable tr' ).removeClass( 'marked' );
 
             tovar.clearObj();
             tovar.clearForma();
@@ -413,120 +356,4 @@ $( document ).ready( function() {
 
     });
 
-
-    // При клике на кнопку добавить
-    // $( '#button_insert' ).on( 'click',
-    //     function () 
-    //     {
-            
-    //         // Очищаем все <span>ы от ошибок
-    //         $(' .msg_error ').text( '' );
-
-    //         // Делаем валидацию для таблицы товаров
-    //         if ( $( '#descr_input' ).val() === '' ) {
-    //             $( '#descr_error' ).text( 'Описание товара не должно быть пустым' );
-    //             $( '#descr_input' ).focus();
-    //             return false;
-    //         };
-
-    //         if ( $( '#descr_input' ).val().length >= 30 ) {
-    //             $( '#descr_error' ).text( 'Описание товара не должно быть слишком длинным' );
-    //             $( '#descr_input' ).focus();
-    //             return false;
-    //         };
-
-    //         if ( $( '#price_input' ).val() === '' ) {
-    //             $( '#price_error' ).text( 'Цена товара должна быть заполнена' );
-    //             $( '#price_input' ).focus();
-    //             return false;
-    //         };
-
-    //         if ( ! $.isNumeric( $( '#price_input' ).val() ) )  {
-    //             $( '#price_error' ).text( 'Цена товара это числовое значение' );
-    //             $( '#price_input' ).focus();
-    //             return false;
-    //         };
-
-    //         if ( + $( '#price_input' ).val() <= 0)  {
-    //             $( '#price_error' ).text( 'Число должно быть положительным' );
-    //             $( '#price_input' ).focus();
-    //             return false;
-    //         };
-
-
-            
-
-
-
-    //         console.log( 'BEGIN------------------------------------------------' );
-
-    //         dataSend = { "firstname": "Pascal", age: 15 };
-
-    //         // $.ajax( {
-    //         //   dataType: "json",
-    //         //   url: 'model/01tovar_ins.php',
-    //         //   data: JSON.stringify( dataSend ),
-    //         //   success: function() {
-    //         //         console.log( 'Ajax запрос выполнился успешно' );
-    //         //     }
-    //         // } );
-
-    //         $.getJSON(
-    //             'model/01tovar_ins.php',
-    //             // { "firstname": "Pascal", "age": 15 },
-    //             // Сериализация процесс перевода структуры данных в последовательность битов.
-    //             // dataSend,
-    //             // Сериализация процесс перевода структуры данных в последовательность битов.
-    //             // dataSend,
-    //             JSON.stringify( dataSend ), 
-    //             function( dataReturned, statusReturned ) {
-    //                 console.log( 'Ajax запрос выполнился успешно' );
-    //             }
-    //         )
-    //             .done(function( json ) {
-    //                 console.log( "JSON Data: " + json );
-                    
-    //                 var items = [];
-    //                 $.each( json, function( key, val ) {
-    //                   items.push( "<li id='" + key + "'>" + val + "</li>" );
-    //                 });
-                    
-    //                 $( "<ul/>", {
-    //                   "class": "my-new-list",
-    //                   html: items.join( "" )
-    //                 }).appendTo( "body" );
-
-    //             })
-    //             .fail(function( jqxhr, textStatus, error ) {
-    //                 var err = textStatus + ", " + error;
-    //                 console.log( "MY Request Failed: " + err );
-    //                 // console.log( "Request Failed: " )
-    //         });
-
-
-    //         console.log( 'END--------------------------------------------------' );
-
-    //         // $.ajax( 'model/01tovar_ins.php',
-    //         // {
-    //         //  type: 'POST',
-    //         //  dataType: 'text', // json в каком формате получать данные
-    //         //  data: 'firstname=Proverka2&age=16',
-    //         //  success: function( mydata ) { 
-    //         //      console.log( 'Ajax-запрос выполнился удачно ###' ); 
-    //         //      console.log( 'От сервера прибыли дынные: ' + mydata ); 
-    //         //  },
-    //         //  error: function( mydata ) { 
-    //         //      console.log( 'Попытка выполнить ajax-запрос провалилась ###' ); 
-    //         //      console.log( 'От сервера прибыли дынные: ' + mydata ); 
-    //         //  }
-    //         // });
-    //     }
-    // );
-
-    //alert(jQuery.fn.jquery);
-
-    // $('table tr').on('click', function(e) {
-    //  $('table tr').removeClass('marked');
-    //  $(this).addClass('marked');
-    // });
 } );
