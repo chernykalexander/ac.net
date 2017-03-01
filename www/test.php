fe858643b29d8cd210ca01616b416fdcef9c106c
chernykalexander <boxtab@bk.ru>
2017-03-01 21:09:11 +0300

    Array test

--- www/test.php
+++ www/test.php
@@ -1,10 +1,29 @@
 <?php 
 
-$MyArray = Array ( 'id' => 1, 'descr' => 'Молоко', 'price' => 15 );
+// function getByIndex($array, $index) {
+// $values = Array ( 'id' => 1, 'descr' => 'Молоко', 'price' => 15 );
+// return $values[$index];
+// }
 
-// echo $MyArray[ 'descr' ];
+// function getByIndex($array, $index) {
+// $value = array_slice($array, $index, 1);
+// return $value[0];
+// }
 
-echo $MyArray[ 1 ];
+// $MyArray = Array ( 'id' => 1, 'descr' => 'Молоко', 'price' => 15 );
+
+// // Так НЕ нужно
+// // echo $MyArray[ 'descr' ];
+
+// // Нужно вот так
+// $tmp = array_slice($MyArray, 2, -1);
+// echo printf( $tmp['price'] );
+
+
+// $arr = ['id' => 1, 'descr' => 'Молоко', 'price' => 15];
+
+// echo $arr[ array_keys($arr)[1] ];
+
 
 exit;
 
